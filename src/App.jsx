454628a7b328e6dc66bda5fc35f7613a00a5684d
Zahra <zahra@device-12.home>
2025-05-09 01:09:39 +0200

    fix: use HashRouter to prevent 404s on Render

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import Signup from './pages/Signup.jsx'
-import Login from './pages/Login.jsx'
-import ProfileForm from './pages/ProfileForm.jsx'
-import MatchResults from './pages/MatchResults.jsx'
+import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Signup from './pages/Signup.jsx';
+import Login from './pages/Login.jsx';
+import ProfileForm from './pages/ProfileForm.jsx';
+import MatchResults from './pages/MatchResults.jsx';
 
 export default function App() {
   return (
@@ -15,10 +15,9 @@ export default function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/profile" element={<ProfileForm />} />
-<Route path="/match/:userId" element={<MatchResults />} />
+          <Route path="/match/:userId" element={<MatchResults />} />
         </Routes>
       </div>
     </Router>
-  )
+  );
 }
-
